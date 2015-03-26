e2963eae6e28033120dc08b287d149b911b79c48
Cas Rusnov <rusnovn@gmail.com>
2015-03-26 03:33:27 -0700

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-openal',
+      version='1.0.0',
+      description='Pythonic bindings for OpenAL',
+      url='https://www.github.com/chaomodus/python-openal',
+      author='Forrest Voight (maintained by Cas Rusnov)',
+      author_email='rusnovn@gmail.com',
+      license='Public Domain',
+      packages=['openal'],
+      zip_safe=True)
